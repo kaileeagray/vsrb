3c88c80d64c8755e897f371d869ee01985890509
Andrew <andcott@gmail.com>
2015-08-21 19:40:58 -0400

    update deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,7 +44,7 @@ set :nginx_use_ssl, false
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
