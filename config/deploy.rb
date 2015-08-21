731a4bc7e4ac452101e7f1f1376220179f998c07
Andrew <andcott@gmail.com>
2015-08-20 22:10:22 -0400

    update deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:andrewcottage/vsrb.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/rails/vsrb'
+set :deploy_to, '/home/deploy/vsrb'
 
 # Default value for :scm is :git
 # set :scm, :git
