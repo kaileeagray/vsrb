ef930343ff693d433703755c5bbe0b3531ffe79b
Andrew <andcott@gmail.com>
2015-08-22 23:33:43 -0400

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,4 +30,4 @@ set :linked_files, fetch(:linked_files, []).push('config/variables.yml')
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
-after "deploy:restart",        "puma:restart"
+after "deploy:update",        "puma:restart"
