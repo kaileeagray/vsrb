5176ed7fc97c9509d32386b5755ff9aa007220a2
Andrew <andcott@gmail.com>
2015-08-23 12:28:08 -0400

    ga code

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,4 @@
 class ContactMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.contact_mailer.form.subject
-  #
   def form(message)
     @greeting = "Hi"
 
